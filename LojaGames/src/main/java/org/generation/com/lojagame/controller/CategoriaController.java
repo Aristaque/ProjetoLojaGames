9e494f42d15385ea56856026b1349368557fbfeb
Fernando Porto <fernandogomes143@gmail.com>
2022-02-24 10:34:51 -0300

    @fernandoportodev [Task: Modificado RequestMapping /categoria]

--- LojaGames/src/main/java/org/generation/com/lojagame/controller/CategoriaController.java
+++ LojaGames/src/main/java/org/generation/com/lojagame/controller/CategoriaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/loja")
+@RequestMapping("/categoria")
 @CrossOrigin("*")
 public class CategoriaController
 {
